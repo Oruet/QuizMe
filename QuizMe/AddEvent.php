6458a72dd0329d569e3ba0263e834acfab1ea570
Fred Vasilescu <sebi_hartie@yahoo.com>
2018-11-24 22:29:59 +0200

    Third

--- QuizMe/AddEvent.php
+++ QuizMe/AddEvent.php
@@ -1,16 +1,19 @@
 <?php
 @mysql_connect("localhost","root","") or die ("Nu se poate conecta la serverul MySQL");
 mysql_select_db("quizme") or die("Nu se poate selecta baza de date");
-echo $Denumire=$_POST['InputDenumire'];
+//echo $ID_Event="11";
+ $Denumire=$_POST['InputDenumire'];
  $Start=$_POST['InputStart'];
- $Stop=$_POST['InputStop'];
- $d1=strtotime("$Start");
- $d2=strtotime("$Stop");
-echo $S1=date("Y-m-d h:m", $Start);
-echo $S2=date("Y-m-d h:m", $Stop);
-echo $Descriere=$_POST['InputDescriere']; 
-$query=mysql_query("insert into Event(Denumire,Start,Stop,Descriere) values('$Denumire','$d1','$d2','$Descriere)"); 
-echo $query;
+  $Stop=$_POST['InputStop'];
+// $d1=strtotime("$Start");
+// $d2=strtotime("$Stop");
+//echo $S1=date("Y-m-d h:m", $Start);
+//echo $S2=date("Y-m-d h:m", $Stop);
+ $Descriere=$_POST['InputDescriere']; 
+$query=@mysql_query("insert into Event  values (NULL,'$Denumire','$Start','$Stop','$Descriere')"); 
+//$query=mysql_query("insert into Event  values('$Denumire','$Start','$Stop','$Descriere')"); 
+
 echo "Eventul a fost adaugat.";
+
 
 ?>
